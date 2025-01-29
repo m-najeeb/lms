0044025ba0fc15af43c72335e1a3896599aefca9
m-najeeb <muhammadnajeeb2022@gmail.com>
2025-01-29 15:57:54 +0500

    building book schema

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// const setup = require("./api/routes");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+logger.token("status-format", (req, res) => {
+  const status = res.statusCode;
+  if (status >= 200 && status < 300) {
+    return "🟢";
+  } else if (status >= 300 && status < 400) {
+    return "🔵";
+  } else {
+    return "🔴";
+  }
+});
+
+app.use(logger(":method :url :status-format :status :response-time ms"));
+
+app.get("/", (req, res) => {
+  res.send("Hello, Server is Running!");
+});
+
+const dbUri = process.env.DB_URI;
+mongoose
+  .connect(dbUri)
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch((error) => {
+    console.log("db error", error);
+    process.exit(1);
+  });
+
+// setup(app);
+
+app.listen(port, () => {
+  console.log(`Server Running on port http://localhost:${port}`);
+});
---
+++ src/models/bookModels.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    isbn: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    publishedYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const BookSchema = mongoose.model("Book", bookSchema);
+module.exports = { BookSchema };
